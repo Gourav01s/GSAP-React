530e6d042efca0be991b79bd1044ce2495b1286f
Gourav01s <gourav8891s@gmail.com>
2026-01-14 21:33:13 +0530

    added hero section and scrollsmoother plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import gsap from "gsap";
 import Navbar from "./components/Navbar";
+import HeroSection from "./sections/HeroSection";
+import MessageSection from "./sections/MessageSection";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <Navbar/>
+      <Navbar />
+      <HeroSection />
+      <MessageSection/>
+      <div className="h-dvh border border-red-600"/>
     </main>
   );
 };
